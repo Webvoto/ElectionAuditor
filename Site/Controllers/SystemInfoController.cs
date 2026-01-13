9c42ff47bc05b30aea998861e776f9001e50b0af
Leonardo Pignataro <leopignataro@gmail.com>
2026-01-13 14:26:01 -0300

    Adicionada rota /api/system/info para visualizar versão da aplicação

---
+++ Site/Controllers/SystemInfoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webvoto.ElectionAuditor.Site.Controllers;
+
+[ApiController]
+[Route("api/system")]
+public class SystemInfoController : ControllerBase {
+
+	public class SystemInfo {
+
+		public string? ProductName { get; set; }
+
+		public string? ProductVersion { get; set; }
+	}
+
+	[AllowAnonymous]
+	[HttpGet("info")]
+	public SystemInfo GetSystemInfo() => new() {
+		ProductName = "Webvoto Election Auditor",
+		ProductVersion = GetType().Assembly.GetName().Version?.ToString(),
+	};
+}
